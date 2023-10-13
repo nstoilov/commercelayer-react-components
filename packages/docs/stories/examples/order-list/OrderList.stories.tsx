5df2c3e8d338fc613522b79b9658b61a7755d8ca
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-10-13 17:42:42 +0200

    docs: fix typos

--- packages/docs/stories/examples/order-list/OrderList.stories.tsx
+++ packages/docs/stories/examples/order-list/OrderList.stories.tsx
@@ -15,38 +15,40 @@ export const Default: StoryFn = (args) => {
   const colClassName = 'uppercase text-left p-1 text-gray-400 text-sm'
   const titleClassName = 'flex flex-row items-center'
 
+  const columns: React.ComponentProps<typeof OrderList>['columns'] = [
+    {
+      header: 'Order',
+      accessorKey: 'number',
+      className: colClassName,
+      titleClassName
+    },
+    {
+      header: 'Status',
+      accessorKey: 'status',
+      className: colClassName,
+      titleClassName
+    },
+    {
+      header: 'Date',
+      accessorKey: 'updated_at',
+      className: colClassName,
+      titleClassName
+    },
+    {
+      header: 'Amount',
+      accessorKey: 'formatted_total_amount_with_taxes',
+      className: colClassName,
+      titleClassName
+    }
+  ]
+
   return (
     <CommerceLayer accessToken='user-access-token'>
       <CustomerContainer>
         <OrderList
           type='orders'
           className='table-fixed w-full border-collapse m-5 '
-          columns={[
-            {
-              header: 'Order',
-              accessorKey: 'number',
-              className: colClassName,
-              titleClassName
-            },
-            {
-              header: 'Status',
-              accessorKey: 'status',
-              className: colClassName,
-              titleClassName
-            },
-            {
-              header: 'Date',
-              accessorKey: 'updated_at',
-              className: colClassName,
-              titleClassName
-            },
-            {
-              header: 'Amount',
-              accessorKey: 'formatted_total_amount_with_taxes',
-              className: colClassName,
-              titleClassName
-            }
-          ]}
+          columns={columns}
           theadClassName='thead-class bg-gray-100 [&>tr>th>span]:flex [&>tr>th>span]:gap-3 [&>tr>th>span]:py-3'
           rowTrClassName='row-tr-class'
         >
@@ -54,23 +56,20 @@ export const Default: StoryFn = (args) => {
 
           <OrderListRow field='number'>
             {({ cell, order, ...p }) => {
-              if (order.type === 'order_subscriptions') {
-                return <div />
-              }
               return (
                 <>
-                  {cell?.map((cell) => {
-                    return (
-                      <div {...p} key={cell.id} className='py-5 border-b'>
-                        <p className='font-bold'>
-                          Order # {cell.renderValue<string>()}
-                        </p>
+                  {cell?.map((cell) => (
+                    <div {...p} key={cell.id} className='py-5 border-b'>
+                      <p className='font-bold'>
+                        Order # {cell.renderValue<string>()}
+                      </p>
+                      {order.type === 'order_subscriptions' ? null : (
                         <p className='text-xs text-gray-500'>
                           contains {order.skus_count} items
                         </p>
-                      </div>
-                    )
-                  })}
+                      )}
+                    </div>
+                  ))}
                 </>
               )
             }}
@@ -88,4 +87,12 @@ export const Default: StoryFn = (args) => {
       </CustomerContainer>
     </CommerceLayer>
   )
+}
+
+Default.parameters = {
+  docs: {
+    source: {
+      type: 'code'
+    }
+  }
 }
